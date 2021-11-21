ea36e12d2085f810687fc2fe4181a29d786c7300
Sergey Zorin <zorinsa92@gmail.com>
2021-11-22 00:51:58 +0300

    Create new project

---
+++ src/main/java/com/zorin/history_testing/HistoryTestingApplication.java
@@ -0,0 +1,13 @@
+package com.zorin.history_testing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HistoryTestingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HistoryTestingApplication.class, args);
+    }
+
+}
